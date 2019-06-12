560997fd526a2a6c944dae6c6295c4329611f584
OlhaMazurenko <mail.olgamazurenko@gmail.com>
2019-06-12 14:48:48 +0300

    'Solution'

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-function daysInYear(year){
+function daysInYear(year) {
   try {
-
-    if(!Number.isInteger(year)) {
-    throw new Error (`exception`);
-  } 
-    let start = new Date(year,0, 0);
-    let end = new Date(year,11,31);
-    let diff = end-start;
-    let days = diff/(1000*60*60*24);
-    return days;
-
-  } catch(e) {
-    return e.message;
+    return daysAmountInYear(year);
+  } catch (error) {
+    return error.message;
   }
-};
- 
- console.log (daysInYear(2019)); // 365
- console.log (daysInYear('2019')); // exception
- console.log (daysInYear(2020)); // 366
- console.log (daysInYear(2020.2)); // exception
- console.log (daysInYear(2100)); // 365
- console.log (daysInYear({year: 2100})); // exception
- 
+}
+
+function daysAmountInYear(year) {
+  if (Number.isInteger(year) && year > 0) {
+    const thisYearEnd = new Date(year + 1, 0, 1);
+    const thisYearStart = new Date(year, 0, 1);
+    const daysAmount = (thisYearEnd.getTime() - thisYearStart.getTime()) / (3600000 * 24);
+    return daysAmount;
+  } else {
+    throw new TypeError("exeption");
+  }
+}
+
+console.log(daysInYear(2019)); // 365
+console.log(daysInYear('2019')); // exception
+console.log(daysInYear(2020)); // 366
+console.log(daysInYear(2020.2)); // exception
+console.log(daysInYear(2100)); // 365
+console.log(daysInYear({ year: 2100 })); // exception
